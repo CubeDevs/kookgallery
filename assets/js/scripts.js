3e1005a409eeaa2f5371d8ba2087460d42f67a35
Hossein Karami <hi@hosseinkarami.com>
2017-06-26 22:11:26 +0430

    🎹  KookGallery 🎸

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * KookGallery.com
+ * Coming Soon page for KookGallery.com
+ * https://HosseinKarami.com
+ * @author Hossein Karami
+ * @version 1.0.5
+ * Copyright 2017. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FastShell
+  });
+
+})(jQuery, window, document);
