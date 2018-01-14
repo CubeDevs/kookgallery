d2af94cb4241f547a1b55152440db66daf3095c3
Hossein Karami <hi@hosseinkarami.com>
2018-01-14 18:12:51 +0330

    test

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,4 +14,40 @@
     // FastShell
   });
 
+
+  var startIncrementingAge = function(shell) {
+      var ageDecimals, decimalsPerSecond;
+      if (!shell) {
+        shell = $('#page');
+      }
+      ageDecimals = $('.age-decimal-increment', shell).first();
+      ageDecimals.css('width', ageDecimals.width());
+      decimalsPerSecond = Math.pow(10, 9) / (365 * 24 * 60 * 60);
+      if (!$('html').hasClass('mobile')) {
+        return gyroscope.aileron.intervals.push(intervalSet(70, (function(_this) {
+          return function() {
+            return _this.updateAgeDecimals($('.age-decimal-increment', shell), decimalsPerSecond);
+          };
+        })(this)));
+      }
+    };
+
+    var updateAgeDecimals = function(element, decimalsPerSecond) {
+      var current, remainder, remainder_string;
+      current = parseFloat(element.first().text());
+      remainder = parseInt(current + decimalsPerSecond * 0.07);
+      if (remainder > Math.pow(10, 9)) {
+        this.incrementBaseAge();
+      }
+      remainder_string = remainder + '';
+      return element.html((' 00000000' + remainder_string).slice(-9));
+    };
+
+    var incrementBaseAge = function() {
+      var current, element;
+      element = $('.incrementable.age-number');
+      current = parseInt(element.html());
+      return element.text(current + 1);
+    };
+
 })(jQuery, window, document);
